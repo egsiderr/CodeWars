d0497bef86dba61052c68c30c4a591ac5316298f
arbuz <egsiderr@gmail.com>
2025-04-05 01:22:51 +0300

    add tasks day 1

---
+++ tests/Highest_and_Lowest.py
@@ -0,0 +1,12 @@
+"""В этом небольшом задании вам дается строка чисел, разделенных пробелами,
+вы должны вернуть наибольшее и наименьшее число.
+Примеры:
+high_and_low("1 2 3 4 5") # return "5 1"
+high_and_low("1 2 -3 4 5") # return "5 -3"
+high_and_low("1 9 3 4 -5") # return "9 -5"""
+
+def high_and_low(numbers):
+    num_list = [int(num) for num in numbers.split()]
+    return f"{max(num_list)} {min(num_list)}"
+
+print(high_and_low("1 2 3 4 5"))
